90c0a903bb391786e7218a5e59342e493cbada5f
ThinkingDove <396593090@qq.com>
2022-01-25 22:35:20 +0800

    p15 Stream的中间操作：映射

--- src/main/java3/StreamAPITest1.java
+++ src/main/java3/StreamAPITest1.java
@@ -4,6 +4,8 @@ import main.java2.Employee;
 import main.java2.EmployeeData;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -37,12 +39,56 @@ public class StreamAPITest1 {
         list.add(new Employee(1010,"刘强东",40,8000));
         list.stream().distinct().forEach(System.out::println);
 
-
-
-
-
-
     }
 
+    //映射
+    @Test
+    public void test2(){
+//        map(Function f)——接收一个函数作为参数，将元素转换为其他形式或提取信息，
+//        该函数会被会被应用到每一个元素上，并将其映射成一个新的元素。
+        List<String> list = Arrays.asList("aa", "bb", "cc", "dd");
+        list.stream().map(str -> str.toUpperCase()).forEach(System.out::println);
+        System.out.println();
+
+//        练习1：获取员工姓名长度大于3的员工的姓名
+        List<Employee> employeeList =EmployeeData.getEmployees();
+        employeeList.stream().map(Employee::getName).filter(name -> name.length() > 3).forEach(System.out::println);
+        System.out.println();
+
+//        练习2:
+        Stream<Stream<Character>> streamStream = list.stream().map(StreamAPITest1::fromStringToStream);
+        streamStream.forEach(s -> {
+            s.forEach(System.out::println);
+        });
+        System.out.println();
+
+//        flatMap(Function f)——接收一个函数作为参数，将流中的每个值都换成另一个流，然后把所有流连接成一个流
+        list.stream().flatMap(StreamAPITest1::fromStringToStream).forEach(System.out::println);
+    }
+
+    //将字符串中的多个字符构成的集合转换为对应的Stream实例
+    public static Stream<Character> fromStringToStream(String str){
+        ArrayList<Character> list = new ArrayList<>();
+        for(Character c : str.toCharArray()){
+            list.add(c);
+        }
+        return list.stream();
+    }
+
+    @Test
+    public void test3(){
+        ArrayList list1 = new ArrayList();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+
+        ArrayList list2 = new ArrayList();
+        list2.add(4);
+        list2.add(5);
+        list2.add(6);
+
+        list1.add(list2);
+        System.out.println(list1);
+    }
 
 }
